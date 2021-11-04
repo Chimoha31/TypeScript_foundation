fb3dd17a48c4a20aa27f7aa54113a8e95ccf10a4
Chimoha31 <mokochii1108@gmail.com>
2021-11-03 19:03:51 -0700

    practice3 & 4 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import React from 'react';
 import { Practice1 } from '../src/practices/Practice1';
 import { Practice2 } from '../src/practices/Practice2';
+import { Practice3 } from '../src/practices/Practice3';
+import { Practice4 } from '../src/practices/Practice4';
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
      <Practice1></Practice1>
      <Practice2></Practice2>
+     <Practice3></Practice3>
+     <Practice4></Practice4>
     </div>
   );
 }
