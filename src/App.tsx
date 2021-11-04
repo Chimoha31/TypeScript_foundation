bb5a43482b8467f7edf2c1a82e3f7db9a8496660
Chimoha31 <mokochii1108@gmail.com>
2021-11-03 23:39:28 -0700

    type for axios & props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Todo } from '../src/Todo';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { Todo } from "../src/Todo";
+import "./App.css";
+// import { type } from "os";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 const App = () => {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
       setTodos(res.data);
-      // console.log(res.data);
     });
   };
 
   return (
     <div className="App">
-     <button onClick={onClickFetchData}>データ取得</button>
-     {todos.map((todo) => (
-       <Todo title={todo.title} userid={todo.userid}></Todo>
-     ))}
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed}></Todo>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
